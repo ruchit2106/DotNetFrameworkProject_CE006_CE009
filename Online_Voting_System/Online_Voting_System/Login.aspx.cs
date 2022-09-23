63dc37e3c165ce242b92a23e475c350b5cfa40d6
ruchit2106 <87220033+ruchit2106@users.noreply.github.com>
2022-09-23 11:39:03 +0530

    Test

--- Online_Voting_System/Online_Voting_System/Login.aspx.cs
+++ Online_Voting_System/Online_Voting_System/Login.aspx.cs
@@ -13,7 +13,7 @@ namespace Online_Voting_System
     public partial class Login : System.Web.UI.Page
     {
         //SqlConnection con = new SqlConnection();
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\admin\source\repos\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ruchit\source\repos\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
 
         protected void Page_Load(object sender, EventArgs e)
         {
--- Online_Voting_System/Online_Voting_System/Register.aspx.cs
+++ Online_Voting_System/Online_Voting_System/Register.aspx.cs
@@ -13,7 +13,7 @@ namespace Online_Voting_System
 
     public partial class Register : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\admin\Documents\GitHub\Online_Voting_System\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ruchit\Documents\GitHub\Online_Voting_System\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
             
