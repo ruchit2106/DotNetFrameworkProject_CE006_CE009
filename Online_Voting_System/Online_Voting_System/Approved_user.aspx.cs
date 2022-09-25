da807afcc377280a93090d0a350a833660464922
Gautam7502 <101510617+Gautam70502@users.noreply.github.com>
2022-09-25 17:27:10 +0530

    Voterslist

--- Online_Voting_System/Online_Voting_System/Approved_user.aspx.cs
+++ Online_Voting_System/Online_Voting_System/Approved_user.aspx.cs
@@ -12,15 +12,58 @@ namespace Online_Voting_System
 {
     public partial class Approved_user : System.Web.UI.Page
     {
-       // SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\admin\Documents\GitHub\Online_Voting_System\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
+        //SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\admin\Documents\GitHub\Online_Voting_System\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
+
+        
+        // SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\admin\Documents\GitHub\Online_Voting_System\Online_Voting_System\Online_Voting_System\App_Data\OnlineVotingSystem.mdf;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
-            String approved_voter = "SELECT * FROM PENDING_VOTERS WHERE IsApproved = @IsApproved";
+            
            
         }
 
-       
+        protected void addvoters_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            //  String approved_voter = "SELECT * FROM PENDING_VOTERS WHERE IsApproved = @IsApproved";
+            string approved_voter = "INSERT INTO VOTERS(Email,Password,DOB,FullName,PhoneNo,Address,Gender) SELECT Email,Password,DOB,FullName,PhoneNo,Address,Gender FROM PENDING_VOTERS WHERE IsApproved = @IsApproved";
+            string delete_pending_voter = "DELETE FROM PENDING_VOTERS WHERE IsApproved = @IsApproved";
+            try
+            {
+                using (con)
+                {
+                    using (SqlCommand cmd = new SqlCommand(approved_voter))
+                    {
+                        cmd.Parameters.AddWithValue("IsApproved", 1);
+
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+
+
+                    using (SqlCommand cmd1 = new SqlCommand(delete_pending_voter))
+                    {
+                        cmd1.Parameters.AddWithValue("IsApproved", 1);
+                        cmd1.Connection = con;
+                        con.Open();
+                        cmd1.ExecuteNonQuery();
+                        con.Close();
+
+                    }
+                }
+
+               
+                    
+                }
+            
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
     }
 }
---
+++ Online_Voting_System/Online_Voting_System/voter_home.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online_Voting_System
+{
+    public partial class voter_home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           print_email.Text = (string)Session["voter_email"];
+            Response.Write(Session["voter_id"]);
+            Response.Write(Session["voter_name"]);
+            Response.Write(Session["Email"]);
+            Response.Write(Session["Password"]);
+            Response.Write(Session["DOB"]);
+            Response.Write(Session["Phoneno"]);
+            Response.Write(Session["Address"]);
+        }
+
+        protected void Edit_Profile_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Edit_Voter_Profile.aspx");
+        }
+
+        protected void logout_button_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void assigned_vote_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
